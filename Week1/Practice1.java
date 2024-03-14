efffc35cdc53b84915ccc5feeffb965745933a55
Tengku Aideed Amsyar <82840182+aideedamsyar@users.noreply.github.com>
2024-03-14 09:55:43 +0900

    💿 Week 1 Lab

---
+++ Week1/Practice1.java
@@ -0,0 +1,10 @@
+package Week1;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        String name = "Tengku Aideed Amsyar Bin Tengku Harizam";
+        String output = "My name is " + name + ", the length of my name is " + name.length();
+
+        System.out.println(output);
+    }
+}
